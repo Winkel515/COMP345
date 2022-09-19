a802cad0782e9b574f1cc6a333f6f966044410b3
Laurent Lao <lao.laurent@gmail.com>
2022-09-19 01:03:22 -0400

    Implement command validation

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -7,33 +7,64 @@ using namespace std;
 
 void displayState(string state) { std::cout << "State at: " << state << "\n"; }
 
-void promptCommand(set<string> validCommands) {
+// Displays the list of valid commands
+void listCommands(set<string> validCommands) {
+  bool firstTime = true;
+
+  std::cout << "List of available commands: {";
+
   for (set<string>::iterator i = validCommands.begin();
        i != validCommands.end(); i++) {
-    std::cout << *i << "\n";
+    if (!firstTime) {
+      std::cout << ", ";
+    } else {
+      firstTime = false;
+    }
+
+    std::cout << *i;
   }
+
+  std::cout << "}.\n";
+}
+
+// Prompts for the correct command
+string promptCommand(set<string> validCommands) {
+  string command = "";
+  listCommands(validCommands);
+
+  do {
+    if (command != "" && validCommands.find(command) == validCommands.end()) {
+      std::cout << "Wrong command. Try again. \n";
+    }
+    std::cout << "Please enter a command: ";
+    std::cin >> command;
+  } while (validCommands.find(command) == validCommands.end());
+
+  return command;
 }
 
 int main() {
   // Map to validate commands per state
-  map<string, set<string>> validCommandsMap;
-  validCommandsMap.insert(pair<string, set<string>>("start", {"loadmap"}));
-  validCommandsMap.insert(
+  map<string, set<string>> validCommands;
+  validCommands.insert(pair<string, set<string>>("start", {"loadmap"}));
+  validCommands.insert(
       pair<string, set<string>>("map_loaded", {"loadmap", "validatemap"}));
-  validCommandsMap.insert(
+  validCommands.insert(
       pair<string, set<string>>("map_validated", {"addplayer"}));
-  validCommandsMap.insert(pair<string, set<string>>(
+  validCommands.insert(pair<string, set<string>>(
       "players_added", {"addplayer", "assigncountries"}));
-  validCommandsMap.insert(
+  validCommands.insert(
       pair<string, set<string>>("assign_reinforcement", {"issueorder"}));
-  validCommandsMap.insert(pair<string, set<string>>(
+  validCommands.insert(pair<string, set<string>>(
       "issue_orders", {"issueorder", "endissueorders"}));
-  validCommandsMap.insert(pair<string, set<string>>(
+  validCommands.insert(pair<string, set<string>>(
       "execute_orders", {"execorder", "endexecorders", "win"}));
-  validCommandsMap.insert(pair<string, set<string>>("win", {"play", "end"}));
-  std::cout << "STARTUP";
+  validCommands.insert(pair<string, set<string>>("win", {"play", "end"}));
+
+  std::cout << "STARTUP\n";
 
   string state = "win";
   displayState(state);
-  promptCommand(validCommandsMap.at(state));
+  string command = promptCommand(validCommands.at(state));
+  std::cout << "You entered: " << command << " \n";
 }
