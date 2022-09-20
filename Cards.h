966a9febdfc3e507ad1e72a2ad7fc599f8208188
NanoProd <joshuajamesnano@gmail.com>
2022-09-19 21:57:07 -0400

    added enum for cardtype

--- Cards.h
+++ Cards.h
@@ -1,38 +1,41 @@
 #ifndef CARDS_H
 #define CARDS_H
 #include <iostream>
+#include <vector>
+
+enum CardType {
+  Bomb,
+  Reinforcement,
+  Blockade,
+  Airlift,
+  Diplomacy
+}
 
 class Card {
  public:
   Card();
 
+ private:
   Card play();
 };
 
-class Bomb : private Card {};
-
-class Reinforcement : private Card {};
-
-class Blockade : private Card {};
-
-class Airlift : private Card {};
-
-class Diplomacy : private Card {};
-
 class Deck {
- private:
-  std::list<Card*> cards;
-
  public:
+  Deck();
+
+ private:
+  std::vector<Card*> cards;
   Card draw();
+  void createDeck();
 };
 
 class Hand {
- private:
-  std::list<Card*> cards;
-
  public:
-  std::list<Card*> getCards();
+  Hand();
+
+ private:
+  std::vector<Card*> cards;
+  std::vector<Card*> fillHand();
 };
 
 #endif
