735c8631b16cbadee7f32c80de9792064b3eb761
Florian Charreau <florian.charreau@me.com>
2022-10-05 13:51:13 -0400

    Added owner and number of armies attributes

--- Map.h
+++ Map.h
@@ -1,42 +1,48 @@
-#include <string>
-#include <vector>
 #include <iostream>
+#include <string>
 #include <unordered_map>
+#include <vector>
 
+class Player;
 using namespace std;
 
-
 class Node {
-    public:
-      string name;
-      vector<Node*> adj;
-      string continent;
-      string color;
-      bool belongsToContinent;
-      Node(string name, string continent);
-      Node(const Node& n);
-      Node& operator=(const Node& n);
-      friend ostream& operator<<(ostream &strm, const Node &n);
+ public:
+  string name;
+  vector<Node*> adj;
+  string continent;
+  string color;
+  int numArmies;
+  Player* owner;
+  void setNumArmies(int _numArmies);
+  int getNumArmies();
+  void setOwner(Player* _owner);
+  Player* getOwner();
+  bool belongsToContinent;
+  Node(string name, string continent);
+  Node(const Node& n);
+  Node& operator=(const Node& n);
+  friend ostream& operator<<(ostream& strm, const Node& n);
 };
 
-
-
 class Map {
-  private:
-    vector<Node*> territories;
-  public:
-    Map(vector<Node*> territories);
-    Map(const Map& m);
-    ~Map();
-    Map& operator=(const Map& m);
-    void printMap();
-    void validate(vector <string> continentsNames);
-    void dfs(int currentNode, vector<Node*> territoriesCopy);
-    void dfs_sub(int currentNode, vector<Node*> continents);
-    void setWhite(vector<Node*> territoriesCopy);
-    void makeBidirectional(vector<Node*> territories);
-    unordered_map<string, vector<Node*>> copyContinents(vector <string> continentsNames, vector<Node*> territoriesCopy);
-    friend ostream& operator<<(ostream &strm, const Map &m);
+ private:
+  vector<Node*> territories;
+
+ public:
+  Map(vector<Node*> territories);
+  Map(const Map& m);
+  ~Map();
+  Map& operator=(const Map& m);
+  void printMap();
+  void validate(vector<string> continentsNames);
+  void dfs(int currentNode, vector<Node*> territoriesCopy);
+  void dfs_sub(int currentNode, vector<Node*> continents);
+  void setWhite(vector<Node*> territoriesCopy);
+  void makeBidirectional(vector<Node*> territories);
+  unordered_map<string, vector<Node*>> copyContinents(
+      vector<string> continentsNames, vector<Node*> territoriesCopy);
+  friend ostream& operator<<(ostream& strm, const Map& m);
 };
 
 string convertAdjToString(vector<Node*> adj);
