230a7bc57a2d68f68d7d9cff9ae7c9dad08cec5f
Florian Charreau <florian.charreau@me.com>
2022-10-31 15:25:47 -0400

    Basics of observer pattern implemented with dummy example

---
+++ LoggingObserver.cpp
@@ -0,0 +1,60 @@
+#include "LoggingObserver.h"
+
+#include <fstream>
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ofstream;
+
+A::A() { a = 0; }
+
+string A::stringToLog() {
+  ofstream output;
+  output.open("gamelog.txt", std::ios_base::app);
+  output << "print to gamelog" << endl;
+  output << "In A string to log" << endl;
+  output.close();
+  cout << "In A string to log" << endl;
+  return "string";
+}
+
+A::A(int a) { a = 1; }
+
+void A::callNotify() { Notify(this); }
+
+B::B() { b = 0; }
+
+B::B(int b) { b = 2; }
+
+Subject::Subject() { _observers = new list<Observer*>; }
+
+Subject::~Subject() { delete _observers; }
+
+Observer::Observer(){};
+
+Observer::~Observer(){};
+
+LogObserver::LogObserver(A* s) {
+  _subject = s;
+  _subject->Attach(this);
+}
+
+void Subject::Attach(Observer* o) { _observers->push_back(o); };
+
+void Subject::Detach(Observer* o) { _observers->remove(o); };
+
+void Subject::Notify(ILoggable* ILog) {
+  list<Observer*>::iterator i = _observers->begin();
+  for (; i != _observers->end(); ++i) (*i)->Update(ILog);
+};
+
+void LogObserver::Update(ILoggable* ILog) { ILog->stringToLog(); }
+
+int main() {
+  std::cout << "bla" << std::endl;
+  A* a1 = new A(1);
+  LogObserver* view = new LogObserver(a1);
+  a1->callNotify();
+
+  return 0;
+}
---
+++ LoggingObserver.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <list>
+#include <string>
+using std::list, std::string;
+
+class ILoggable {
+ public:
+  virtual string stringToLog() = 0;
+};
+
+class Observer {
+ public:
+  ~Observer();
+  virtual void Update(ILoggable* ILog) = 0;
+
+ protected:
+  Observer();
+};
+
+class Subject {
+ public:
+  virtual void Attach(Observer* o);
+  virtual void Detach(Observer* o);
+  virtual void Notify(ILoggable* ILog);
+  Subject();
+  ~Subject();
+
+ private:
+  list<Observer*>* _observers;
+};
+
+class A : public Subject, public ILoggable {
+ public:
+  ~A();
+  A();
+  int a;
+  A(int a);
+  string stringToLog();
+  void callNotify();
+};
+
+class B {
+ public:
+  ~B();
+  B();
+  int b;
+  B(int b);
+};
+
+class LogObserver : public Observer {
+ public:
+  LogObserver();
+  LogObserver(A* s);
+  ~LogObserver();
+  void Update(ILoggable* ILog);
+
+ private:
+  A* _subject;
+};
